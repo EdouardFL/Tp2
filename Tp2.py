4d99a98fed521ea3b6e5930a1c913bbbbe5d041e
EdouardFL <112488051+EdouardFL@users.noreply.github.com>
2022-11-08 09:21:07 -0500

    Update Tp2.py

--- Tp2.py
+++ Tp2.py
@@ -3,45 +3,56 @@
 import random
 
 min = 0
-max = 10
+max = 100
 
 reponse = 0 #La réponse
 essaies = 0 #Le nombre d'essaies
 
 def initiate(): #fonction qui initialize le jeu
+    global min,max
+    min = int(input("Choisizer une borne minimale"))
+    max = int(input("Choisizer une borne maximale"))
+
     global reponse
     reponse = random.randint(min, max)  # Choisi un nombre entre le min et le max
     print("J'ai choisi un nombre entre",min,max,".")
-    guessfunc()
 
 def guessfunc(): #Fonction qui permet au joueur de deviner
     guess = input("Essayer de deviner ce nomber:")
     guess = int(guess)
-    global essaies #globalise la variable essaies
+    #globalise la variable essaies
 
     if guess > reponse: #Si la réponse est plus grande que la réponse:
         print("Votre reponse est plus grande que le nombre mysterieux !!")
-        essaies += 1
-        guessfunc()
+        return("Incorrect")
 
     elif guess < reponse: #Si la réponse est plus petit que la réponse:
         print("Votre reponse est plus petit que le nombre mysterieux ")
-        essaies += 1
-        guessfunc()
+        return("Incorrect")
 
     elif guess == reponse: #Si la réponse est egale que la réponse:
-        essaies += 1
         print("VOUS AVEZ EU LA BONNE REPONSE YOUPI !!!!")
-        print("Cela vous a prit:",essaies,"essaies")
-        restart()
+        return("Correct")
 
 def restart(): #Re-initialze le code is le joueur veux
     retry = input("Voulez vous re-jouez ???????? (O/N)")
-    if retry == "O" or retry == "o":
-        initiate()
-        global essaies #globalise la réponse
-        essaies = 0
-    else:
-        print("Au revoir !!")
+    return retry
 
 initiate()
+
+playing = True
+
+while playing:
+    guess = guessfunc()
+    essaies += 1
+
+    if guess == "Correct":
+        print("Cela vous a prit:", essaies, "essaies")
+        rstart = restart()
+        if rstart == "O" or rstart == "o":
+            essaies = 0
+            initiate()
+        else:
+            print("Au revoir !!")
+            playing = False
+
